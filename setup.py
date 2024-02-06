143df56df24d1f17f9e4e2afbab963a22611f1ca
John <normlman@live.com>
2024-02-06 08:06:33 -0600

    carbon

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="asynchronizer",
-    version="0.1.1",
+    name="pyscynchronize",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
 
